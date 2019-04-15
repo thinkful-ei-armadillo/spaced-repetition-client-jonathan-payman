498881a5e53eca20eb10cd9e69002c99abe1be96
Jonathan Kleinpeter <joklein341@gmail.com>
2019-04-15 15:04:41 -0500

    small change to fragment

--- src/components/WordList/WordList.js
+++ src/components/WordList/WordList.js
@@ -3,17 +3,17 @@ import React from 'react';
 export function WordList(props) {
   const { words } = props;
   return (
-    <ul className='word-list'>
+    <>
       <li>{ words }</li>
-    </ul>
+    </>
   )
 }
 
 export function WordListResults(props) {
   const { correct_count, incorrect_count } = props;
   return (
-  <ul className='word-list-results'>
+  <>
     <li></li>
-  </ul>
+  </>
   )
 }
