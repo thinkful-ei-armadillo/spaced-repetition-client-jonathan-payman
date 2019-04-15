a04ab2a9f0ceb57b7ec4a373a140eabd0f280b07
Payman Kossari <Paymankossari@gmail.com>
2019-04-15 12:45:49 -0400

    fix header a bit

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,57 +1,50 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
-import TokenService from '../../services/token-service'
-import UserContext from '../../contexts/UserContext'
-import './Header.css'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import TokenService from '../../services/token-service';
+import UserContext from '../../contexts/UserContext';
+import './Header.css';
 
 class Header extends Component {
-  static contextType = UserContext
+  static contextType = UserContext;
 
   handleLogoutClick = () => {
-    this.context.processLogout()
-  }
+    this.context.processLogout();
+  };
 
   renderLogoutLink() {
     return (
-      <div className='navbar'>
-        <span>
-          {this.context.user.name}
-        </span>
-        <nav>
-          <Link
-            onClick={this.handleLogoutClick}
-            to='/login'>
-            Logout
-          </Link>
-        </nav>
-      </div>
-    )
+      <nav>
+        <span>{this.context.user.name}</span>
+
+        <Link onClick={this.handleLogoutClick} to="/login">
+          Logout
+        </Link>
+      </nav>
+    );
   }
 
   renderLoginLink() {
     return (
-      <nav className='navbar'>
-        <Link to='/login'>Login</Link>
-        {' '}
-        <Link to='/register'>Sign up</Link>
+      <nav>
+        <Link to="/login">Login</Link> <Link to="/register">Sign up</Link>
       </nav>
-    )
+    );
   }
 
   render() {
     return (
       <header>
-        {TokenService.hasAuthToken()
-          ? this.renderLogoutLink()
-          : this.renderLoginLink()}
-        <h1>
-          <Link to='/'>
-            Spaced repetition
-          </Link>
-        </h1>
+        <div className="navbar">
+          {TokenService.hasAuthToken()
+            ? this.renderLogoutLink()
+            : this.renderLoginLink()}
+          <h1 className="nav-header">
+            <Link to="/">Spaced repetition</Link>
+          </h1>
+        </div>
       </header>
     );
   }
 }
 
-export default Header
+export default Header;
