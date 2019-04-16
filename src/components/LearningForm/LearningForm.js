28a5f3ba4911f6c1e65ee1686c78c12c422ff550
Jonathan Kleinpeter <joklein341@gmail.com>
2019-04-16 16:49:05 -0500

    style changes to learningForm

--- src/components/LearningForm/LearningForm.js
+++ src/components/LearningForm/LearningForm.js
@@ -3,10 +3,10 @@ import { Input, Required, Label } from '../Form/Form';
 import Button from '../Button/Button';
 import learningApiService from '../../services/learning-api-service';
 import LanguageContext from '../../contexts/LanguageContext';
+import './LearningForm.css';
 
 export default function LearningForm(props) {
   const languageContext = useContext(LanguageContext);
-  //const [wordInput, updateWord] = useState(null);
   const [head, setHead] = useState(null);
   const questionInput = useRef(null);
 
@@ -20,24 +20,28 @@ export default function LearningForm(props) {
   const guessWord = e => {
     e.preventDefault();
     console.log(questionInput.current.value);
-    //questionInput.current;
   };
 
   return (
     <React.Fragment>
       {head !== null && (
-        <form onSubmit={e => guessWord(e)}>
-          {/* <input type='text' required /> */}
-          <h2>Translate the word:</h2>
-          <span>{head.nextWord}</span>
-          <Input
-            ref={questionInput}
-            id="learning-question-input"
-            name="question"
-            required
-          />
-          <Button type="submit">Submit</Button>
-        </form>
+        <>
+          { console.log(head) }
+          <h2>Translate the word: {head.nextWord}</h2>
+          <form onSubmit={e => guessWord(e)}>
+            <p className='word-form-info'>What's the Translation for this word?</p>
+            <p className='word-form-info'>Correctly translated this word { head.wordCorrectCount } times</p>
+            <p className='word-form-info'>Incorrect translation for this word { head.wordIncorrectCount } times</p>
+            <p className='word-form-info'>Score: { head.totalScore }</p>
+            <Input
+              ref={questionInput}
+              id='learning-question-input'
+              name='question'
+              required
+            />
+            <Button type='submit'>Submit</Button>
+          </form>
+        </>
       )}
     </React.Fragment>
   );
