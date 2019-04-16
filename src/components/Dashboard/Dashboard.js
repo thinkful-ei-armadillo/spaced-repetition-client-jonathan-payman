088c50344dd119d671ab12b9a575406e956d4385
Payman Kossari <Paymankossari@gmail.com>
2019-04-15 22:20:30 -0400

    style for dashboard

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import UserContext from '../../contexts/UserContext';
 import LanguageContext from '../../contexts/LanguageContext';
 import dashboardApiService from '../../services/dashboard-api-service';
 import { WordListResults, WordList } from '../WordList/WordList';
 import Button from '../Button/Button';
-import './Dashboard.css'
+import './Dashboard.css';
 
 class Dashboard extends Component {
   state = {
@@ -30,21 +31,25 @@ class Dashboard extends Component {
         {value => {
           return (
             <div>
-              <h2>{value.user.name}'s Dashboard</h2>
-              <h3>{this.context.language.name}</h3>
-              <h4>Words</h4>
-              <section className='list-container'>
-              <ul className='word-list'>
-                {
-                  words.map((word, i) => <WordList key={i} word={word} />)
-                }
-              </ul>
-              <ul className='word-results'>
-                {
-                  words.map((word, i) => <WordListResults key={i} word={word} />)
-                }
+              {/*<h1>{value.user.name}'s Dashboard</h1>*/}
+              <h2>{this.context.language.name}</h2>
+              <p>Total correct answers: {this.context.language.total_score}</p>
+
+              <section className="list-container">
+                <h3>Words to practice</h3>
+                <ul className="word-list">
+                  {words.map((word, i) => (
+                    <WordList key={i} word={word} />
+                  ))}
                 </ul>
-                <Button className='dashboard-btn'>Start Practicing</Button>
+                {/*<ul className="word-results">
+                  {words.map((word, i) => (
+                    <WordListResults key={i} word={word} />
+                  ))}
+                </ul>*/}
+                <Link to="/learn" className="dashboard-btn">
+                  <Button className="dashboard-btn">Start practicing</Button>
+                </Link>
               </section>
             </div>
           );
--- src/components/WordList/WordList.js
+++ src/components/WordList/WordList.js
@@ -2,19 +2,19 @@ import React from 'react';
 
 export function WordList(props) {
   const { word } = props;
-  return (
-    <li>
-      {word.original}
-    </li>
-  )
-}
-
-export function WordListResults(props) {
   const { correct_count, incorrect_count } = props.word;
   const total = correct_count + incorrect_count;
   return (
-    <li>
-      {correct_count} / {total}
+    <li className="word-list-item">
+      <h4>{word.original}</h4>
+      <span>
+        <div>correct answer count: {correct_count}</div>
+        <div>incorrect answer count: {incorrect_count}</div>
+      </span>
     </li>
   );
+}
+
+export function WordListResults(props) {
+  return <li />;
 }
