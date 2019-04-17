6fbeac169c75e5c4cdfa91aa16641575cb78e83c
Jonathan Kleinpeter <joklein341@gmail.com>
2019-04-17 10:14:41 -0500

    add errorboundary component

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import { UserProvider } from './contexts/UserContext'
 import App from './components/App/App'
+import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary';
 import './setup-icons'
 import './index.css'
 import * as serviceWorker from './serviceWorker'
@@ -11,7 +12,9 @@ import * as serviceWorker from './serviceWorker'
 ReactDOM.render(
   <BrowserRouter>
     <UserProvider>
-      <App />
+      <ErrorBoundary>
+        <App />
+      </ErrorBoundary>
     </UserProvider>
   </BrowserRouter>,
   document.getElementById('root'),
